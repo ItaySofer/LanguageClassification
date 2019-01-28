b509e70bad26e8afdde48bc6b0a5b6969f7949ac
Itay Sofer <ItaySofer@users.noreply.github.com>
2019-01-28 22:06:48 +0200

    name change

--- src/dataload/tweet_transforms.py
+++ src/dataload/tweet_transforms.py
@@ -15,40 +15,41 @@ class RemoveTokenBase(object):
     def __init__(self):
         pass
     
-    def __call__(self, word_list):
-        return [word for word in word_list if self.should_remove(word) is False]
+    def __call__(self, token_list):
+        return [token for token in token_list if self.should_remove(token) is False]
 
     @abstractmethod
-    def should_remove(self, word):
+    def should_remove(self, token):
         pass
 
 
 class RemoveURLs(RemoveTokenBase):
-    def should_remove(self, s):
-        parseResult = urlparse(s)
+    def should_remove(self, token):
+        parseResult = urlparse(token)
         return parseResult.scheme != '' or parseResult.netloc != ''
 
 
 class RemoveMentions(RemoveTokenBase):
-    def should_remove(self, s):
-        return s.startswith('@')
+    def should_remove(self, token):
+        return token.startswith('@')
 
 
 class RemoveResponseToken(RemoveTokenBase):
-    def should_remove(self, s):
-        return s == "RT"
+    def should_remove(self, token):
+        return token == "RT"
 
 
 class RemoveHashtags(RemoveTokenBase):
-    def should_remove(self, s):
-        return s.startswith('#')
+    def should_remove(self, token):
+        return token.startswith('#')
 
 
 class RemoveBlanks(RemoveTokenBase):
-    def should_remove(self, s):
-        return s == ""
+    def should_remove(self, token):
+        return token == ""
 
 
 class RemoveEmojis(RemoveTokenBase):
-    def should_remove(self, s):
-        return all(c in emoji.UNICODE_EMOJI for c in s)
+    def should_remove(self, token):
+        return all(c in emoji.UNICODE_EMOJI for c in token)
+
