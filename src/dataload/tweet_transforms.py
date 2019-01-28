97deda38576c4732e71e1589f62f3a6bee9887c9
Itay Sofer <ItaySofer@users.noreply.github.com>
2019-01-28 21:29:13 +0200

    added splitToWords and RemoveURLs tweet transforms

---
+++ src/dataload/tweet_transforms.py
@@ -0,0 +1,26 @@
+from urllib.parse import urlparse
+
+
+class SplitToWords(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, tweet):
+        return tweet.split(" ")
+
+
+class RemoveURLs(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, word_list):
+        return [word for word in word_list if RemoveURLs.is_url(word) is False]
+
+    @staticmethod
+    def is_url(s):
+        parseResult = urlparse(s)
+        return parseResult.scheme != '' or parseResult.netloc != ''
+
+
