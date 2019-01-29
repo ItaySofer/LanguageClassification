72f802a2c5130c97cf4326cb16e2d4792aa3daf9
Itay Sofer <ItaySofer@users.noreply.github.com>
2019-01-29 19:56:41 +0200

    concatenated all language datasets to one

--- src/main.py
+++ src/main.py
@@ -5,23 +5,33 @@ from src.dataload.label_transforms import ToOneHot
 from src.model.language_classifier import LanguageClassifier
 import torch
 import torch.nn as nn
+from torch.utils.data import ConcatDataset
 
 
-tweet_transform = transforms.Compose(
-    [SplitToWords(),
-     RemoveURLs(),
-     RemoveMentions(),
-     RemoveResponseToken(),
-     RemoveHashtags(),
-     CleanToken(),
-     RemoveBlanks(),
-     ToLowerCase()])
-label_transform = ToOneHot(language_names=['en', 'es', 'fr', 'in', 'it', 'nl', 'pt', 'tl'])
-data = TweetsDataset(root_dir='../data', language_name='en',
-                     tweet_transform=tweet_transform, label_transform=label_transform)
-for i in range(10):
-    print(data[i])
+def prepare_data():
+    language_names = ['en', 'es', 'fr', 'in', 'it', 'nl', 'pt', 'tl']
 
+    tweet_transform = transforms.Compose(
+        [SplitToWords(),
+         RemoveURLs(),
+         RemoveMentions(),
+         RemoveResponseToken(),
+         RemoveHashtags(),
+         CleanToken(),
+         RemoveBlanks(),
+         ToLowerCase()])
+    label_transform = ToOneHot(language_names=language_names)
+
+    language_datasets = []
+    for language_name in language_names:
+        dataset = TweetsDataset(root_dir='../data', language_name=language_name,
+                                tweet_transform=tweet_transform, label_transform=label_transform)
+        language_datasets.append(dataset)
+
+    return ConcatDataset(language_datasets)
+
+
+data = prepare_data()
 
 classifier = LanguageClassifier(num_of_classes=8)
 pred = classifier(data[0]['tweet'])
